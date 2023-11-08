84e27fa8cb2be47a3f60dcef760659a05231ae38
Eduarda <eduardabpetry@gmail.com>
2023-11-08 11:52:47 +0100

    Add function to handle number

--- 1.Guessing/script.js
+++ 1.Guessing/script.js
@@ -3,32 +3,28 @@ const secretNumber = () => {
     return number.toString();
 }
 
-
 let winnerNumber = secretNumber();
-console.log(winnerNumber);
 let missedNumberPlus = parseFloat(winnerNumber) + 1;
-console.log(missedNumberPlus);
 let missedNumberMinus = parseFloat(winnerNumber) - 1;
-console.log(missedNumberMinus);
 
 const handleNumber = (e) => {
     let guessedNumber = document.getElementById("guessedNumber").value;
     if (guessedNumber == 0 || guessedNumber > 26) {
         alert ("This is a impossible number. Try a different one!")
+        e.preventDefault();
     }
     else if (guessedNumber === winnerNumber) {
         alert("You won! But whoops... I guess I ate it!")
     }
     else if (guessedNumber === missedNumberPlus.toString() || guessedNumber === missedNumberMinus.toString()) {
         alert("So close! It almost seems like you don't want to win...")
+        e.preventDefault();
     }
     else if (guessedNumber !== winnerNumber){
         alert("Nope. Try again!")
+        e.preventDefault();
    }
-   console.log(guessedNumber);
-   e.preventDefault();
 }
-
 
 const button = document.querySelector("button");
 button.addEventListener("click", handleNumber);
