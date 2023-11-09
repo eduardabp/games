666e63f52367457c54d535d4b383918edc8051c8
Eduarda <eduardabpetry@gmail.com>
2023-11-09 11:42:16 +0100

    Add function to calculate results

--- 3.21card/script.js
+++ 3.21card/script.js
@@ -20,8 +20,6 @@ playButton.addEventListener("click", () => {
 })
 
 /* player */
-    
-    /* table */
 
 let playerScore = 0
 const playerHand = () => {
@@ -41,8 +39,6 @@ const playerHand = () => {
 }
 
 /* computer */
-
-    /* table */
 
 let computerScore = 0
 const computerHand = () => {
@@ -66,30 +62,58 @@ const computerHand = () => {
     computerCount.innerHTML = computerScore;
 }
 
-  /* button hit me */
-
-  const hitMe = document.getElementById("hit-me");
-  hitMe.addEventListener("click", playerHand);
-  hitMe.addEventListener("click", computerHand);
-
-  /* stop */
-
-
 /* results */
 
-    /* player above 21 */
+const calculateResults = () => {
+    const scoreboard = document.querySelector(".result");
+    const result = document.createElement("p");
+        /* player or both above 21 - table wins*/
+    if (playerScore > 21) {
+        const text = document.createTextNode("You busted! I hope you haven't bet your savings on this game...");
+        result.appendChild(text);
+    }
+        /* table above 21 and player below/equal 21 - player wins*/
+    else if (computerScore > 21 && playerScore < 22) {
+        const text = document.createTextNode("Who you are gonna call? Computerbusters! You won!");
+        result.appendChild(text);
+    }
+        /* player 21 or below and table has less points */
+    else if (playerScore < 22 && playerScore > computerScore) {
+        const text = document.createTextNode("You won! Wait, were you counting cards? Hum...Security!!");
+        result.appendChild(text);
+    }
+        /* table 21 or below and player has less points */
+    else if (computerScore < 22 && computerScore > playerScore) {
+        const text = document.createTextNode("Better luck next time. Computer wins!");
+        result.appendChild(text);
+    }
+        /* tie on 21 or below*/ 
+    else if (playerScore < 22 && computerScore < 22 && playerScore === computerScore) {
+        const text = document.createTextNode("It's a... tie? That's not fun. Play again!");
+        result.appendChild(text);
+    }
+    scoreboard.appendChild(result);
+}
+    
+/* button hit me */
 
-    /* table above 21 */
+const hitMe = document.getElementById("hit-me");
+hitMe.addEventListener("click", playerHand);
+hitMe.addEventListener("click", computerHand);
 
-    /* player wins count */
+/* button stop */
 
-    /* table wins count */
+const stop = document.getElementById("stop");
+stop.addEventListener("click", () => {
+  hitMe.disabled = true;
+  while (computerScore < 16) {
+      computerHand();
+  }
+  calculateResults();
+  stop.disabled = true;
+})
 
-    /* both above 21 - table wins */ 
-
-    /* tie */ 
-
-    /* play again */
+    /* button play again */
 
 
 
